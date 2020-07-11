f9cb40b1e999d51ee549e3b63c9741051ac098f8
JeremyBrent <jeremybrentdata@gmail.com>
2020-07-10 23:33:00 -0400

    commented javascript

--- Assets/js/file.js
+++ Assets/js/file.js
@@ -1,3 +1,4 @@
+// Linked with css to change navbar color when scrolling past 400px on the page
 $(window).scroll(function(){
 	$('.navbar-default').toggleClass('scrolled', $(this).scrollTop() > 400);
 });
